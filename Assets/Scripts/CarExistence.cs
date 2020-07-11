c8646483b04099ab7d1eb24bd794ca78644b3a57
CrazyShroob <47101684+CrazyShroob@users.noreply.github.com>
2020-07-11 14:29:26 +0200

    Hackerman Ivan miraculously managed to implement lives and gameovers. How can a single man this intelligent? Scientists are still looking for an answer, but will they ever find it? Tune in on our next episode of National Geographic to find out more.

---
+++ Assets/Scripts/CarExistence.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CarExistence : MonoBehaviour
+{
+    public int lives;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check to see if the tag on the collider is equal to Enemy
+        if (other.gameObject.tag == "Wall")
+        {
+            lives--;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(lives <= 0){
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
