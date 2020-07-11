41342592a527fd516e82abd5cff43bc2ac342788
Lionel Karlen <lionel.karlen@protonmail.com>
2020-07-11 16:18:03 +0800

    Camera Fixure, Added Sprites, Preparing for procedural generation

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject car;
+    public GameObject mainCamera;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        mainCamera.transform.position=new Vector3(car.transform.position.x,car.transform.position.y,-10f);
+        // transform.position=Vector3.Lerp(transform.position,mainCamera.transform.position,speed*Time.deltaTime);
+    }
+}
