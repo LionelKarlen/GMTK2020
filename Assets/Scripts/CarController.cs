7abc56d3084f1d9d1492f37ad7aa38da696fe085
CrazyShroob <47101684+CrazyShroob@users.noreply.github.com>
2020-07-11 12:53:09 +0200

    Deceleration WOW

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -6,6 +6,7 @@ public class CarController : MonoBehaviour {
 
     public Rigidbody2D rb;
     public float speed;
+    public float deceleration;
 
     // Start is called before the first frame update
     void Start() {
@@ -16,5 +17,8 @@ public class CarController : MonoBehaviour {
     void FixedUpdate() {
         rb.AddForce(Input.GetAxisRaw("Vertical")*speed*transform.up);
         transform.Rotate(new Vector3(0f,0f,.5f), Input.GetAxisRaw("Horizontal")*speed*-1);
+        if(Input.GetKey("up") == false && Input.GetKey(KeyCode.W) == false){
+            rb.velocity = rb.velocity * deceleration;
+        }
     }
 }
