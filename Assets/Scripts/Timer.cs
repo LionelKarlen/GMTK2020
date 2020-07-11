06b8684322e4b3dc1dfde379b786d9c3273f1c53
CrazyShroob <47101684+CrazyShroob@users.noreply.github.com>
2020-07-11 15:49:46 +0200

    Il timerone

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    private float startTime;
+
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Time.time - startTime;
+        string Minutes = ((int) t/60).ToString();
+        string Seconds = (t % 60).ToString("f2");
+        if((t % 60) <= 9.99f){
+            timerText.text = Minutes + ":0" + Seconds; 
+        }
+        else{
+        timerText.text = Minutes + ":" + Seconds;
+        } 
+    }
+}
