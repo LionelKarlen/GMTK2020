48a812537863047532906c52e6c2b24cc9f0bfeb
Lionel Karlen <lionel.karlen@protonmail.com>
2020-07-12 16:03:39 +0800

    Added 2d extras package, finished road generation

--- Assets/Scripts/Map/MapGeneration.cs
+++ Assets/Scripts/Map/MapGeneration.cs
@@ -12,7 +12,7 @@ public class MapGeneration : MonoBehaviour {
     public Tilemap obstacleTilemap;
 
     public Tile grass;
-    public Tile[] roads;
+    public RuleTile roads;
     public Tile[] obstacles;
 
     public int distanceModifier;
@@ -24,6 +24,8 @@ public class MapGeneration : MonoBehaviour {
     private Vector3 startPointVector;
     private Vector3 endPointVector;
 
+    private Vector2[] offsets = {new Vector2(0,-1), new Vector2(-1,0), new Vector2(0,1), new Vector2(1,0)};
+
     // Start is called before the first frame update
     void Start() {
         for(int x=0;x<size.x;x++) {
@@ -31,12 +33,12 @@ public class MapGeneration : MonoBehaviour {
                 Vector3Int tile = new Vector3Int(x,y,0);
                 grassTilemap.SetTile(tile, grass);
                 if(x<size.x*.25f && Random.Range(0f,1f)>.75f && !startPoint) {
-                    roadTilemap.SetTile(tile, roads[0]);
+                    roadTilemap.SetTile(tile, roads);
                     startPointVector=tile;
                     startPoint=true;
                     CameraRig.transform.position = roadTilemap.CellToWorld(tile)+new Vector3(3.5f,3.5f,0);
                 } else if(x>size.x*.9f && Random.Range(0f,1f)>.75f && !endPoint && y>startPointVector.y+distanceModifier) {
-                    roadTilemap.SetTile(tile, roads[0]);
+                    roadTilemap.SetTile(tile, roads);
                     endPointVector=tile;
                     endPoint=true;
                 }
@@ -45,13 +47,14 @@ public class MapGeneration : MonoBehaviour {
                 }
             }
         }
-        foreach(Vector2 road in findRandomPath(new Vector2(startPointVector.x,startPointVector.y),new Vector2(endPointVector.x,endPointVector.y))) {
-            roadTilemap.SetTile(new Vector3Int((int)road.x,(int)road.y,0), roads[0]);
+        List<Vector2> path = findRandomPath(new Vector2(startPointVector.x,startPointVector.y),new Vector2(endPointVector.x,endPointVector.y));
+        foreach(Vector2 road in path) {
+            Debug.Log(road);
+            roadTilemap.SetTile(new Vector3Int((int)road.x,(int)road.y,0), roads);
         }
     }
 
     List<Vector2> findRandomPath(Vector2 start, Vector2 end) {
-        Vector2[] offsets = {new Vector2(0,-1), new Vector2(-1,0), new Vector2(0,1), new Vector2(1,0)};
         int possible_moves = offsets.Length;
 
         Vector2 current_position = start;
@@ -81,4 +84,31 @@ public class MapGeneration : MonoBehaviour {
         }
         return false;
     }
+
+    // Tile getTileType(Vector2 coordinate, Tilemap tilemap) {
+    //     Debug.Log(tilemap.GetTile(new Vector3Int((int)coordinate.x+1,(int)coordinate.y,0)));
+    //     if(tilemap.HasTile(tilemap.WorldToCell(tilemap.CellToWorld(new Vector3Int((int)coordinate.x+1,(int)coordinate.y,0))))) {
+    //         if(tilemap.HasTile(new Vector3Int((int)coordinate.x,(int)coordinate.y+1,0))) {
+    //             return roads[1];
+    //         } else if(tilemap.HasTile(new Vector3Int((int)coordinate.x,(int)coordinate.y-1,0))) {
+    //             return roads[1];
+    //         }
+    //         return roads[2];
+    //     } else if(tilemap.HasTile(tilemap.WorldToCell(tilemap.CellToWorld(new Vector3Int((int)coordinate.x-1,(int)coordinate.y,0))))) {
+    //         if(tilemap.HasTile(new Vector3Int((int)coordinate.x,(int)coordinate.y+1,0))) {
+    //             return roads[1];
+    //         } else if(tilemap.HasTile(new Vector3Int((int)coordinate.x,(int)coordinate.y-1,0))) {
+    //             return roads[1];
+    //         }
+    //         return roads[2];
+    //     }
+    //     // } else if(tilemap.HasTile(new Vector3Int((int)coordinate.x,(int)coordinate.y+1,0))) {
+            
+        //     return roads[0];
+        // } else if(tilemap.HasTile(new Vector3Int((int)coordinate.x,(int)coordinate.y-1,0))) {
+            
+        //     return roads[0];
+        // }
+        // return roads[0];
+    // }
 }
