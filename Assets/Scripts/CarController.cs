df44f65719a94cdf0a3b8d6d449bacdaeb967251
CrazyShroob <47101684+CrazyShroob@users.noreply.github.com>
2020-07-12 07:36:09 +0200

    Yo did he he just add sound effects PogChamp?????

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -7,10 +7,11 @@ public class CarController : MonoBehaviour {
     public Rigidbody2D rb;
     public float speed;
     public float deceleration;
+    AudioSource CarSounds;
 
     // Start is called before the first frame update
     void Start() {
-        
+        CarSounds = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -19,6 +20,12 @@ public class CarController : MonoBehaviour {
         transform.Rotate(new Vector3(0f,0f,.5f), Input.GetAxisRaw("Horizontal")*speed*-1);
         if(Input.GetKey("up") == false && Input.GetKey(KeyCode.W) == false){
             rb.velocity = rb.velocity * deceleration;
+        }
+        if(Input.GetKey(KeyCode.W)||Input.GetKey("up")){
+            CarSounds.Play(0);
+        }
+        else{
+            CarSounds.Pause();
         }
     }
 }
---
+++ Assets/Scripts/LevelCountText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelCountText : MonoBehaviour
+{
+    static public int LevelCount = 0;
+    public Text LevelCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.T)){
+            LevelCount++;
+        }
+        LevelCounter.text = LevelCount.ToString();
+    }
+}
