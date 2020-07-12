a835b5ad36965b8ac1ee0df248af77d655eda36a
CrazyShroob <47101684+CrazyShroob@users.noreply.github.com>
2020-07-12 12:41:19 +0200

    NEW INNOVATION: THE RETRY BUTTON ONCE YOU DIE

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,6 +6,6 @@ using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
     public void PLayGame(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("PlayableScene");
     }
 }
